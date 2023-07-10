8f3d38247b7d62170e292b760b8eca52a70346b6
AlekseyKashubin <Akashubin@yahoo.com>
2023-07-10 13:53:14 -0700

    pokemon_api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './components/post';
+import { useState } from 'react';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState([])
+
+
+  const getList = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=808&offset=0')
+      .then(res => res.json())
+      .then((data)=> setPokemonName(data))
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Pokemon List</h1>
+
+
+    <button onClick={getList} className='bigButton' >Catch em All</button>
+
+    <Post name={pokemonName}/>
+    
     </div>
   );
 }
